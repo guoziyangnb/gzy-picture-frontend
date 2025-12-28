91db11f5455344d37d7b070c0fb05503af141c70
gzynb <2263831821@qq.com>
2025-12-29 01:14:33 +0800

    feat：图片批量编辑功能完成

--- src/pages/SpaceDetailPage.vue
+++ src/pages/SpaceDetailPage.vue
@@ -13,6 +13,14 @@
         <a-button type="primary" :href="`/add_picture?spaceId=${id}`" target="_blank">
           + 创建图片
         </a-button>
+        <a-button
+          type="primary"
+          style="background-color: orange"
+          :icon="h(EditOutlined)"
+          @click="doBatchEdit"
+        >
+          批量编辑</a-button
+        >
         <a-tooltip
           :title="`当前占用空间 ${formatSize(space.totalSize)} / ${formatSize(space.maxSize)}`"
         >
@@ -43,11 +51,17 @@
       :show-total="() => `图片总数 ${total} 条/ ${space.maxCount}`"
       @change="onPageChange"
     />
+    <BatchEditPictureModal
+      ref="batchEditPictureModalRef"
+      :spaceId="id"
+      :pictureList="dataList"
+      :onSuccess="onBatchEditPictureSuccess"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, h } from 'vue'
 import { message } from 'ant-design-vue'
 import { getSpaceVoByIdUsingGet } from '@/service/api/spaceController'
 import {
@@ -59,6 +73,8 @@ import PictureList from '@/components/PictureList.vue'
 import PictureSearchForm from '@/components/PictureSearchForm.vue'
 import { ColorPicker } from 'vue3-colorpicker'
 import 'vue3-colorpicker/style.css'
+import BatchEditPictureModal from '@/components/BatchEditPictureModal.vue'
+import { EditOutlined } from '@ant-design/icons-vue'
 
 interface Props {
   id: string | number
@@ -150,6 +166,22 @@ const onColorChange = async (color: string) => {
     message.error('获取数据失败，' + res.data.message)
   }
   loading.value = false
+}
+
+// -------------批量编辑图片----------------
+// 分享弹窗引用
+const batchEditPictureModalRef = ref()
+
+// 批量编辑成功后，刷新数据
+const onBatchEditPictureSuccess = () => {
+  fetchData()
+}
+
+// 打开批量编辑弹窗
+const doBatchEdit = () => {
+  if (batchEditPictureModalRef.value) {
+    batchEditPictureModalRef.value.openModal()
+  }
 }
 </script>
 
